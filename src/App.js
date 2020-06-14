8a83154560cb7fff5d43b56af9a07db00e4271cc
Abid Jaweed <52130668+Abidjaweed@users.noreply.github.com>
2020-06-14 21:29:19 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionList } from './components/TransactionList';
+import { AddTrasaction } from './components/AddTrasaction';
+
+import { GlobalProvider } from './context/GlobalState'
+
+
+import './App.css';
+
+function App() {
+  return (
+    <GlobalProvider>
+      <Header />
+      <div className="container">
+      <Balance />
+      <IncomeExpenses />
+      <TransactionList />
+      <AddTrasaction />
+      </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
